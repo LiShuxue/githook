720a45eabe6ebae9bdc1a7230fbba225d1532634
LiShuxue <1149926505@qq.com>
2019-12-15 13:28:50 +0800

    构建journey-client之前先关闭一些服务来腾出内存

--- automation-job/journey-client.js
+++ automation-job/journey-client.js
@@ -4,6 +4,11 @@ const logger = require('../utils/logger');
 const journeyClientBuildAndDeploy = async () => {
   try {
     logger.info('Journey-client automation job start...');
+    
+    // 先关闭一些服务来腾出内存， 关闭后台服务，关闭MongoDB服务
+    await myShell.exec('mongod --shutdown');
+    await myShell.exec('pm2 stop journey-server');
+
     await myShell.cd('/root/.jenkins/workspace/journey-client');
     await myShell.exec('git checkout .');
     await myShell.exec('git pull');
@@ -12,6 +17,11 @@ const journeyClientBuildAndDeploy = async () => {
     await myShell.rm('-rf', 'dist/js/*.map');
     await myShell.rm('-rf', '/root/project/journey/journey-client/*');
     await myShell.cp('-r', 'dist/*', '/root/project/journey/journey-client/');
+
+    // 再重启刚才关闭的服务。后台服务，MongoDB服务
+    await myShell.exec('mongod -f /data/db/mongodb.cnf');
+    await myShell.exec('pm2 start journey-server');
+
     await myShell.exec('nginx -s reload');
     logger.info('Journey-client automation job successful.');
     return Promise.resolve();
