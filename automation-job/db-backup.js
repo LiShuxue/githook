b36bec358c680f963b6ee728e82768aa72bf51a4
LiShuxue <1149926505@qq.com>
2019-12-17 16:22:45 +0800

    DB备份到七牛云

--- automation-job/db-backup.js
+++ automation-job/db-backup.js
@@ -1,10 +1,13 @@
 const myShell = require('./myShell');
 const logger = require('../utils/logger');
+const qiniuUtil = require('../utils/qiniuUtil');
+const moment = require('moment');
 
 const dbBackupAndUpload = async () => {
   try {
     logger.info('DB-Backup automation job start...');
-    await myShell.cd('/root/db-backup');
+    let dbBackupPath = '/root/db-backup/'
+    await myShell.cd(dbBackupPath);
     // 数据库备份
     await myShell.exec('mongodump -h localhost:27017 -d journey -o /root/db-backup -u journey -p journey');
 
@@ -12,9 +15,13 @@ const dbBackupAndUpload = async () => {
     // await myShell.exec('mongorestore -h localhost:27017 -d journey /root/db-backup/journey -u journey -p journey');
 
     // 压缩数据库备份文件
-    await myShell.exec('zip -r journey-`date +%Y-%m-%d-%H-%M-%S`.zip journey');
+    let time = moment().format('YYYY-MM-DD-HH-mm-ss');
+    let fileName = `DB-journey-${time}.zip`;
+    await myShell.exec(`zip -r ${fileName} journey`);
 
     // 上传至七牛云
+    let filePath = `${dbBackupPath}${fileName}`;
+    await qiniuUtil.fileUpload(fileName, filePath);
     logger.info('DB-Backup automation job successful.');
     return Promise.resolve();
   } catch (err) {
---
+++ utils/qiniuUtil.js
@@ -0,0 +1,55 @@
+// 服务器端上传文件到七牛。客户端的上传参考journey项目
+
+const qiniu = require('qiniu');
+const logger = require('./logger');
+
+// 鉴权对象
+const accessKey = 'uHIW2IbsCKWoeaEW3x5tX6ajX3xL010MmmWar5vC';
+const secretKey = 'BJzBW7iaoRMh370HdlWSI4gzjL9tbkn-J19uzedC';
+const mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+const bucket = 'journey-db';
+
+// 设置上传到的云空间的存储区域，华北
+const config = new qiniu.conf.Config();
+config.zone = qiniu.zone.Zone_z1;
+
+// 获取上传凭证
+const uploadToken = () => {
+  let options = {
+    scope: bucket, // 存储空间的Bucket名字
+    expires: 1 * 60 // 上传凭证的过期时间，单位s
+  };
+  let putPolicy = new qiniu.rs.PutPolicy(options);
+  return putPolicy.uploadToken(mac);
+}
+
+// 文件上传
+const fileUpload = (fileName, filePath) => {
+  logger.info('Start upload the db to Qiniuyun...');
+  logger.info('fileName: ' + fileName);
+  logger.info('filePath: ' + filePath);
+
+  let key = 'blog/mongodb/' + fileName;
+  let formUploader = new qiniu.form_up.FormUploader(config);
+  let putExtra = new qiniu.form_up.PutExtra();
+
+  return new Promise((resolve, reject) => {
+    formUploader.putFile(uploadToken(), key, filePath, putExtra, (respErr, respBody, respInfo) => {
+      if (respErr) {
+        logger.error('Upload failed!');
+        logger.error(respErr);
+        reject(respErr);
+      }
+
+      logger.info('Upload successful.');
+      resolve({
+        respBody,
+        respInfo
+      })
+    });
+  })
+}
+
+module.exports = {
+  fileUpload
+};
